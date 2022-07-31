125744d7b1cc90f522483e498a5319b0b13fffe1
donghun93 <alswn4516@gmail.com>
2022-07-31 16:34:43 +0900

    feat: coffee service init

---
+++ src/main/java/com/story/coffeeservice/CoffeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.story.coffeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/story/coffeeservice/CoffeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.story.coffeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
